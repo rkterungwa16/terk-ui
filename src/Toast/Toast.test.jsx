c70cab2c78c38082f18fb4e5e488fa62f7234a24
rkterungwa16 <terungwakombol@gmail.com>
2020-09-05 08:14:37 +0100

    [test] toast message

--- src/Toast/Toast.test.jsx
+++ src/Toast/Toast.test.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { ToastMessage } from "./ToastMessage";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<ToastMessage />);
-  const linkElement = getByText(/learn react/i);
+const remove = jest.fn();
+test("renders toast message", () => {
+  const { getByText } = render(
+    <ToastMessage remove={remove}>Hello there</ToastMessage>
+  );
+  const linkElement = getByText(/Hello there/i);
   expect(linkElement).toBeInTheDocument();
 });
