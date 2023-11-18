ff7e2cac4103f9d383288e5e42da56281099e0ec
rkterungwa16 <terungwakombol@gmail.com>
2023-11-18 15:41:06 +0100

    implement custom link component

---
+++ packages/terkui-core/src/Link/Link.tsx
@@ -0,0 +1,39 @@
+import { forwardRef } from "react";
+import classNames from "classnames";
+import { LinkProps } from "./types";
+
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+  (
+    { children, active, className, customLink, disabled, href, ...rest },
+    ref,
+  ) => {
+    if (customLink) {
+      const customLinkProps = customLink.props;
+      const Component = customLink.component;
+      return (
+        <Component
+          className={classNames(className, { active, disabled })}
+          {...(active && { "aria-current": "page" })}
+          {...(disabled && { "aria-disabled": true, tabIndex: -1 })}
+          {...rest}
+          ref={ref}
+          href={href}
+          {...customLinkProps}
+        >
+          {children}
+        </Component>
+      );
+    }
+    return (
+      <a
+        className={classNames(className, { active, disabled })}
+        {...(active && { "aria-current": "page" })}
+        {...(disabled && { "aria-disabled": true, tabIndex: -1 })}
+        {...rest}
+        ref={ref}
+      >
+        {children}
+      </a>
+    );
+  },
+);
---
+++ packages/terkui-core/src/Link/types.ts
@@ -0,0 +1,18 @@
+import { AllHTMLAttributes, ElementType } from "react";
+
+export interface LinkAttrProps extends AllHTMLAttributes<HTMLElement> {
+  active?: boolean;
+  className?: string;
+  disabled?: boolean;
+  href?: string;
+}
+
+export type CustomLinkProps = Record<string, unknown>;
+export interface LinkProps
+  extends LinkAttrProps,
+    AllHTMLAttributes<HTMLElement> {
+  customLink?: {
+    component: ElementType;
+    props: CustomLinkProps;
+  };
+}
