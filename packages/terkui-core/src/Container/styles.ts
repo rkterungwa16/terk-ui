4cc09f658d14ed6b4cc2f8bf8c10cb9b69a81a93
rkterungwa16 <terungwakombol@gmail.com>
2023-11-20 17:15:59 +0100

    fix typo

--- packages/terkui-core/src/Container/styles.ts
+++ packages/terkui-core/src/Container/styles.ts
@@ -5,8 +5,8 @@ import { devices } from "../utils/breakpoints/breakpoints";
 
 export const breakpointWidth = {
   /**
-   * For all with greater than sm, the width should will not exceed 540px
-   * For with less than or equal to sm the width is 100%;
+   * For all width greater than sm, the width should will not exceed 540px
+   * For width less than or equal to sm the width is 100%;
    */
   sm: `
     width: 100%;
@@ -15,8 +15,8 @@ export const breakpointWidth = {
     }
   `,
   /**
-   * For all with greater than md, the width should will not exceed 720px
-   * For with less than or equal to md the width is 100%;
+   * For all width greater than md, the width should will not exceed 720px
+   * For width less than or equal to md the width is 100%;
    */
   md: `
     width: 100%;
@@ -25,8 +25,8 @@ export const breakpointWidth = {
     }
   `,
   /**
-   * For all with greater than lg, the width should will not exceed 960px
-   * For with less than or equal to lg the width is 100%;
+   * For all width greater than lg, the width should will not exceed 960px
+   * For width less than or equal to lg the width is 100%;
    */
   lg: `
     width: 100%;
@@ -35,8 +35,8 @@ export const breakpointWidth = {
     }
   `,
   /**
-   * For all with greater than xl, the width should will not exceed 1140px
-   * For with less than or equal to xl the width is 100%;
+   * For all width greater than xl, the width should will not exceed 1140px
+   * For width less than or equal to xl the width is 100%;
    */
   xl: `
     width: 100%;
@@ -45,8 +45,8 @@ export const breakpointWidth = {
     }
   `,
   /**
-   * For all with greater than 2xl, the width should will not exceed 1320px
-   * For with less than or equal to 2xl the width is 100%;
+   * For all width greater than 2xl, the width should will not exceed 1320px
+   * For width less than or equal to 2xl the width is 100%;
    */
   "2xl": `
     width: 100%;
